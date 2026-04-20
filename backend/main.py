0df30fb21b79ad502ec49b7ebb0d1e808d89c2dd
Arianna Sinnato <arisinnato@MacBook-Neo-de-Arianna.local>
2026-04-20 15:56:08 -0400

    commit de creacion de ruta de categoria

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 import models
-from database import engine
+from database import engine, get_db
 
 app = FastAPI(title="Nacre API")
 
-# Esta línea crea las tablas en Postgres al iniciar el servidor
 models.Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def read_root():
     return {"message": "Bienvenida a la API de Nacre, Ari"}
-    
+
+@app.get("/productos")
+def obtener_productos(db: Session = Depends(get_db)):
+    productos = db.query(models.Product).all()
+    return productos
+
+#ruta para ver producto por categoria (aun en prueba)
+@app.get("/productos/categoria/{cat_id}")
+def productos_por_categoria(cat_id: int, db: Session = Depends(get_db)):
+    return db.query(models.Product).filter(models.Product.category_id == cat_id).all()
