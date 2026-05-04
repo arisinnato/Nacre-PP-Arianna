e8af42361b79270b6d90f232b19e7523fe72f44b
Arianna Sinnato <arisinnato@aris-neo.local>
2026-05-04 09:07:12 -0400

    cambio en contraseña en auth_routes

--- backend/routes/auth_routes.py
+++ backend/routes/auth_routes.py
@@ -3,7 +3,6 @@ from sqlalchemy.orm import Session
 from database import get_db
 import models, auth, schemas
 
-
 router = APIRouter(
     prefix="/auth",
     tags=["Autenticación"]  
@@ -14,8 +13,10 @@ def register_user(user: dict, db: Session = Depends(get_db)):
     db_user = db.query(models.User).filter(models.User.username == user['username']).first()
     if db_user:
         raise HTTPException(status_code=400, detail="El usuario ya existe")
-        
-    hashed_password = auth.get_password_hash(user['password'])
+    
+    raw_password = str(user['password'])[:72]
+    hashed_password = auth.get_password_hash(raw_password)
+    
     new_user = models.User(
         username=user['username'], 
         email=user.get('email'), 
@@ -29,7 +30,9 @@ def register_user(user: dict, db: Session = Depends(get_db)):
 def login(user: dict, db: Session = Depends(get_db)):
     db_user = db.query(models.User).filter(models.User.username == user['username']).first()
     
-    if not db_user or not auth.verify_password(user['password'], db_user.password):
+    raw_password = str(user['password'])[:72]
+    
+    if not db_user or not auth.verify_password(raw_password, db_user.password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, 
             detail="Datos incorrectos"
